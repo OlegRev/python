0ecf4fe6337e271cf0b967a620cd486b08244427
OlegRev <olegglukharev92@gmailcom>
2019-04-13 21:00:50 +0300

    ls4 start

---
+++ lesson4/ls4.py
@@ -0,0 +1,19 @@
+﻿n1 = 1
+n2 = n1
+n2 = 4
+print(n1)
+test1 = [1, 2, 3, 4, 5]
+test2 = test1
+test2.append("STROKA")
+print(test1)
+test2=test1[:]
+test2.append('NOVAi')
+print(test1)
+test2 = test1.copy()
+test2.append("NEW")
+print(test1)
+print(test2)
+test2=test1
+nom1 = 22
+num2 = num1
+
